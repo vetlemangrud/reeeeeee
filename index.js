0ed4194729e67dff4504403c40bcd43ceba86564
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-01-24 17:01:45 +0100

    Test

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 const dotenv = require('dotenv');
+const express = require("express");
+const bodyParser = require('body-parser');
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
+
+
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.send(req.body)
+})
+
+app.post("/", (req, res) => {
+    res.send(req.body);
+})
+  
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`)
+})
