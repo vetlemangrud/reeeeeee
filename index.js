60ee9fb28b41c01d470fa725a01584d19847afca
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-01-24 17:35:22 +0100

    Pass challenge

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const dotenv = require('dotenv');
 const express = require("express");
 const bodyParser = require('body-parser');
 const app = express();
+app.use( bodyParser.json() );
 app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
 
 app.get('/', (req, res) => {
-    res.send(req.body)
+    res.send(req.query)
 })
 
 app.post("/", (req, res) => {
-    res.send(req.body);
+    res.send(req.body.challenge);
 })
   
 app.listen(process.env.PORT, () => {
