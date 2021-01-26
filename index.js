fbc35d246a017dea369f7d89b535d492cf1158fd
Vetle Roos Mangrud <vmangrud@gmail.com>
2021-01-26 21:50:28 +0100

    Small test

--- index.js
+++ index.js
@@ -3,15 +3,18 @@ const { App } = require('@slack/bolt');
 dotenv.config();
 
 const app = new App({
-  signingSecret: process.env.SLACK_SIGNING_SECRET,
-  token: process.env.SLACK_BOT_TOKEN,
-}); 
-
-/* Add functionality here */
-
+    signingSecret: process.env.SLACK_SIGNING_SECRET,
+    token: process.env.SLACK_BOT_TOKEN
+});
+  
+  
+app.event('app_home_opened', ({ event, say }) => {  
+    say('Helo');
+});
+  
+  
+// Start your app
 (async () => {
-  // Start the app
-  await app.start(process.env.PORT || 3000);
-
-  console.log('⚡️ Bolt app is running!');
+    await app.start(process.env.PORT || 3000);
+    console.log('⚡️ Bolt app is running!');
 })();
